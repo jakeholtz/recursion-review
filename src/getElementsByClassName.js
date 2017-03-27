c2a1a4240f569f409312b3b83e3df5bba26ad826
Hack Reactor Student <student@hackreactor.com>
2017-03-27 12:33:11 -0700

    WORKING on getElementsByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,36 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var getElementsByClassName = function(className, node
 ) {
-  // your code here
+	var elements = [];
+	// //check if a node is passed in as an argument
+	// node = document.body || node;
+	// // store string of classnames into a variable
+	// var parts = node.classname.split(' '); //can we split html collection into a string
+	// //check if there's a match of classname in parts
+	// if(parts.indexof(className) >= 0) {
+	// 	nodes.push(node);
+	// }
+	// for(var i = 0; i<node.children.length; i++) {
+	// 	getElementsByClassName(node, className);
+	// }
+	// starting at document.body
+	var node = document.body || node;
+	for (var i = 0; i<node.length; i++){
+
+      _.contains(node.classList, className) ? elements.push(node) : null;
+      getElementsByClassName(className, child)
+	
+
+}
+   
+      
+// 	if document.body.childNodes[0] !== undefined
+// 	   go through each element and if classname is className push it to nodes
+	return elements;
 };
+
+
+console.log(document.getElementsByClassName("targetClassName"));
+console.log(getElementsByClassName("targetClassName"));
