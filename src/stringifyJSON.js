2da345d65ddf10eb8bee5fdf5ba89fc9b18ea89b
Hack Reactor Student <student@hackreactor.com>
2017-03-27 18:51:55 -0700

    UPDATED stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,6 +5,18 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === "boolean" || typeof obj === "number"){
+  	return obj.toString();
+  } else if (typeof obj ==="string"){
+  	return '"' +obj+ '"';
+  } else if (Array.isArray(obj)){
+  	var array = [];
+  	_.each(obj, function (element){
+  		array.push(stringifyJSON(element));
+  	});
+  	// return "[" + array.split(",") + "]";
+  	return JSON.stringify(array)
+  }
   if(obj === null){
     return "null";
   }
